cc17a7255f5e8b12a5dcdf344599b3d61d43e023
Lucas Warner <rdandmclover123@gmail.com>
2024-12-01 21:17:00 -0500

    PPO works.... kinda

--- PPO/drone_env.py
+++ PPO/drone_env.py
@@ -1,20 +1,37 @@
 import gym
 from gym import spaces
+import random
 import numpy as np
+from OpenGL.GL import *
+from OpenGL.GLU import *
 from stable_baselines3 import PPO
 from drone import Drone
+from obstacle import Obstacle
+from spatial_grid import SpatialGrid
+
+ROTATION_SPEED = 3
+MAX_VELOCITY = 0.2
+ACCELERATION = 0.01
+DECELERATION = 0.005
+SIZE = 0.5
+BOUNDARY_TOLERANCE = 0
+
+
 
 class DroneEnv(gym.Env):
     def __init__(self):
-        super().__init__()\
-
-        self.drone = Drone()
-
+        super().__init__()
+        self.goal_position = np.array([50,3,50])
+        self.drone = Drone([0, 2, 0], [0.0, 0.0, 0.0], 0)
+        self.obstacles = self.generate_obstacles(45)
+        self.spatial_grid = SpatialGrid(cell_size=10)
+        for obstacle in self.obstacles:
+            self.spatial_grid.add_obstacle(obstacle)
         # State space: [x, y, z, heading, vx, vy, vz, angular_vel]
         self.observation_space = gym.spaces.Box(
             low=-np.inf,
             high=np.inf,
-            shape=(8,),
+            shape=(7,),
             dtype=np.float32
         )
         # Define action space for all possible movements
@@ -24,79 +41,235 @@ class DroneEnv(gym.Env):
         # 5: move fwd    6: move back
         # 7: rotate left 8: rotate right
         self.action_space = gym.spaces.Discrete(9)
-
+    """
     def reset(self, seed=None):
-        # Reset drone to initial state using its own reset method
-        self.drone.reset()  # Implement this in your Drone class if not exists
-        return self.get_observation(), {}
+    # Reset drone to initial state using its own reset method
+    self.drone.reset()  # Implement this in your Drone class if not exists
+    return self.get_observation(), {}
+    """
+
+    def check_collision(self, radius):
+
+        drone_min_x, drone_max_x = self.drone.position[0] - (SIZE / 4 + BOUNDARY_TOLERANCE), \
+                                   self.drone.position[0] + (SIZE / 4 + BOUNDARY_TOLERANCE)
+        drone_min_y, drone_max_y = self.drone.position[1], self.drone.position[1] + (
+                SIZE / 2 + BOUNDARY_TOLERANCE)
+        drone_min_z, drone_max_z = self.drone.position[2] - (SIZE / 2 + BOUNDARY_TOLERANCE), \
+                                   self.drone.position[2] + (SIZE / 2 + BOUNDARY_TOLERANCE)
+
+        obstacle_min_x, obstacle_max_x = -100, 100
+        obstacle_min_y, obstacle_max_y = -1, 0
+        obstacle_min_z, obstacle_max_z = -100, 100
+
+        if (drone_min_x <= obstacle_max_x and drone_max_x >= obstacle_min_x) and \
+                (drone_min_y <= obstacle_max_y and drone_max_y >= obstacle_min_y) and \
+                (drone_min_z <= obstacle_max_z and drone_max_z >= obstacle_min_z):
+            return True
+
+        # Assuming `detect_nearby_obstacles` returns a list of Obstacle objects
+        obstacles = self.spatial_grid.detect_nearby_obstacles(self.drone, SIZE, radius)
+
+        for obstacle in obstacles:
+            # Access the position and size attributes directly from the Obstacle object
+            obstacle_x, obstacle_y, obstacle_z = obstacle.position
+            obstacle_size = obstacle.size
+
+            obstacle_min_x, obstacle_max_x = obstacle_x - (obstacle_size / 2 + BOUNDARY_TOLERANCE), obstacle_x + (
+                    obstacle_size / 2 + BOUNDARY_TOLERANCE)
+            obstacle_min_y, obstacle_max_y = 0, obstacle_y + BOUNDARY_TOLERANCE
+            obstacle_min_z, obstacle_max_z = obstacle_z - (obstacle_size / 2 + BOUNDARY_TOLERANCE), obstacle_z + (
+                    obstacle_size / 2 + BOUNDARY_TOLERANCE)
+
+            if (drone_min_x <= obstacle_max_x and drone_max_x >= obstacle_min_x) and \
+                    (drone_min_y <= obstacle_max_y and drone_max_y >= obstacle_min_y) and \
+                    (drone_min_z <= obstacle_max_z and drone_max_z >= obstacle_min_z):
+                return True
+        return False
 
     def get_observation(self):
         # Use drone's actual state including velocity and acceleration
-        return np.array([
-            self.drone.x,
-            self.drone.y,
-            self.drone.z,
-            self.drone.heading,
-            self.drone.velocity_x,
-            self.drone.velocity_y,
-            self.drone.velocity_z,
-        ], dtype=np.float32)
+        return np.array([self.drone.position[0],self.drone.position[1],self.drone.position[2],
+                self.drone.heading,
+                self.drone.velocity[0], self.drone.velocity[1], self.drone.velocity[2]])
 
     def move_up(self):
-        self.drone.move_up()
+        delta_v = [self.drone.velocity[0], self.drone.velocity[1] + ACCELERATION, self.drone.velocity[2]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+        self.drone.update_position(DECELERATION, True, self.check_collision(5))
 
     def move_down(self):
-        self.drone.move_down()
+        delta_v = [self.drone.velocity[0], self.drone.velocity[1] - ACCELERATION, self.drone.velocity[2]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+        self.drone.update_position(DECELERATION, True, self.check_collision(5))
 
-    def move_left(self):
-        self.drone.move_left()
+    def move_left(self, rotation_matrix):
+        delta_a = np.matmul(rotation_matrix, [-ACCELERATION, 0])
+        delta_v = [self.drone.velocity[0] + delta_a[0], self.drone.velocity[1], self.drone.velocity[2] + delta_a[1]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+        self.drone.update_position(DECELERATION, True, self.check_collision(5))
 
-    def move_right(self):
-        self.drone.move_right()
+    def move_right(self, rotation_matrix):
+        delta_a = np.matmul(rotation_matrix, [ACCELERATION, 0])
+        delta_v = [self.drone.velocity[0] + delta_a[0], self.drone.velocity[1], self.drone.velocity[2] + delta_a[1]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+        self.drone.update_position(DECELERATION, True, self.check_collision(5))
 
-    def move_forward(self):
-        self.drone.move_forward()
+    def move_forward(self, rotation_matrix):
+        delta_a = np.matmul(rotation_matrix, [0, -ACCELERATION]).tolist()
+        delta_v = [self.drone.velocity[0] + delta_a[0], self.drone.velocity[1], self.drone.velocity[2] + delta_a[1]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+        self.drone.update_position(DECELERATION, True, self.check_collision(5))
 
-    def move_backward(self):
-        self.drone.move_backward()
+    def move_backward(self, rotation_matrix):
+        delta_a = np.matmul(rotation_matrix, [0, ACCELERATION])
+        delta_v = [self.drone.velocity[0] + delta_a[0], self.drone.velocity[1], self.drone.velocity[2] + delta_a[1]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+        self.drone.update_position(DECELERATION, True, self.check_collision(5))
 
     def rotate_left(self):
-        self.drone.rotate_left()
+        self.drone.heading -= ROTATION_SPEED
+        self.drone.update_position(DECELERATION, False, self.check_collision(5))
 
     def rotate_right(self):
-        self.drone.rotate_right()
+        self.drone.heading += ROTATION_SPEED
+        self.drone.update_position(DECELERATION, False, self.check_collision(5))
 
     def step(self, action):
+        rotation_matrix = np.array([[np.cos(np.radians(self.drone.heading)), -np.sin(np.radians(self.drone.heading))],
+                                    [np.sin(np.radians(self.drone.heading)), np.cos(np.radians(self.drone.heading))]])
+
         # Apply the action using drone's existing methods
         if action == 1:
             self.move_up()
         elif action == 2:
             self.move_down()
         elif action == 3:
-            self.move_left()
+            self.move_left(rotation_matrix)
         elif action == 4:
-            self.move_right()
+            self.move_right(rotation_matrix)
         elif action == 5:
-            self.move_forward()
+            self.move_forward(rotation_matrix)
         elif action == 6:
-            self.move_backward()
+            self.move_backward(rotation_matrix)
         elif action == 7:
             self.rotate_left()
         elif action == 8:
             self.rotate_right()
-
-        # Update drone physics (assuming your Drone class has an update method)
-        self.drone.update()  # This should handle acceleration/deceleration
 
         # Get observation after action
         obs = self.get_observation()
 
         # Calculate reward
         reward = 0  # Define your reward function
+        # Calculate the direction to the goal
+        prev_distance_to_goal = self.goal_position - self.drone.last_position
+        prev_distance_to_goal = np.array([prev_distance_to_goal[0], prev_distance_to_goal[2]])
+        distance_to_goal = self.goal_position - self.drone.position
+        distance_to_goal = np.array([distance_to_goal[0], distance_to_goal[2]])
+        direction_to_goal = distance_to_goal / np.linalg.norm(distance_to_goal)  # Normalize the vector
+        print(direction_to_goal)
+        # Calculate the drone's heading vector
+        heading_radians = np.radians(self.drone.heading)
+        drone_heading_vector = np.array([-np.cos(heading_radians), np.sin(heading_radians)])
+        drone_heading_vector /= np.linalg.norm(drone_heading_vector)
+        print(drone_heading_vector)
+        # Calculate alignment using dot product and velocity using norm
+        alignment = np.dot(drone_heading_vector, direction_to_goal)
+        velocity_magnitude = np.linalg.norm(self.drone.velocity)
+        # Reward for heading towards the goal
+        reward += alignment * 2  # Scale the reward as needed
+        reward += pow(np.linalg.norm(distance_to_goal), -1)
+        # if alignment correct increase reward
+        print(self.drone.position)
+        if np.linalg.norm(distance_to_goal) - np.linalg.norm(prev_distance_to_goal) < -.2:
+            reward += .5
+        else:
+            reward += -.1
+        # if velocity less then 55% correct then decrease reward
+        if np.dot([self.drone.velocity[0], self.drone.velocity[2]], direction_to_goal) < .55:
+            reward += -velocity_magnitude * 5
+            print("not correct velocity vector")
+        # Reward for reaching the goal
+        if np.linalg.norm(self.drone.position - self.goal_position) < SIZE:
+            reward += 100.0
+        # if drone is too low command it to go up
+        if self.drone.position[1] < 2.5:
+            print("drone too low!")
+            reward += self.drone.position[1] - 2.5
+        # if alignment and velocity vector are correct increase reward
+        if alignment > 0.9 and np.dot([self.drone.velocity[0], self.drone.velocity[2]], direction_to_goal) > .75:
+            reward += alignment + velocity_magnitude * 20
+            print("aligned and proceeding in correct direction")
+        # if drone is too high make it go down
+        if self.drone.position[1] > 10:
+            print("drone too high!")
+            reward += -8.0
+            # Favor action 8
+        if self.drone.position[0] > 90:
+            print("drone too far!")
+            reward += -10.0
+            # Favor action 8
+        if self.drone.position[0] < -90:
+            print("drone too far!")
+            reward += -12.0
+            # Favor action 8
+        if self.drone.position[2] > 90:
+            print("drone too far!")
+            reward += -14.0
+            # Favor action 8
+        if self.drone.position[2] < -90:
+            print("drone too far!")
+            reward += -16.0
+            # Favor action 8
 
+        if self.spatial_grid.nearby_obstacles(self.drone, SIZE, 2):
+            print("nearby obstacle detected")
+            reward += -2.0
+        if alignment > 0.999:
+            print("correct alignment")
+            reward += 10.0
+
+        print(reward)
         # Check if episode is done
         done = False  # Define your termination conditions
+        info = {}
 
         return obs, reward, done, False, {}
     def render(self, mode='human'):
         self.game.run()
+
+    @staticmethod
+    def draw_ground_plane():
+        glColor4f(0.5, 0.5, 0.5, 0.5)
+        glBegin(GL_QUADS)
+        glVertex3f(-100, 0, -100)
+        glVertex3f(100, 0, -100)
+        glVertex3f(100, 0, 100)
+        glVertex3f(-100, 0, 100)
+        glEnd()
+
+    @staticmethod
+    def draw_goal_box():
+        glColor4f(1, 1, 0, 1)
+        glBegin(GL_QUADS)
+        glVertex3f(55, 0, 45)
+        glVertex3f(55, 0, 55)
+        glVertex3f(45, 0, 45)
+        glVertex3f(55, 0, 45)
+        glVertex3f(55, 5, 45)
+        glVertex3f(55, 5, 55)
+        glVertex3f(45, 5, 45)
+        glVertex3f(55, 5, 45)
+        glEnd()
+
+    @staticmethod
+    def generate_obstacles(num_obstacles):
+        obstacles = []
+        for _ in range(num_obstacles):
+            size = random.uniform(0.5, 2.5)
+            height = random.uniform(2, 10.0)
+            x = random.uniform(-50, 50)
+            z = random.uniform(-50, 50)
+            obstacle = Obstacle([x, height, z], size)
+            obstacles.append(obstacle)
+        return obstacles
---
+++ PPO/game.py
@@ -0,0 +1,180 @@
+import pygame
+from pygame.locals import DOUBLEBUF, OPENGL
+import random
+import numpy as np
+from drone import Drone
+from drone_env import DroneEnv
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from stable_baselines3 import PPO
+
+WIDTH, HEIGHT = 800, 600
+SIZE = 0.5
+ROTATION_SPEED = 3
+ACCELERATION = 0.01
+DECELERATION = 0.005
+MAX_VELOCITY = 0.2
+BOUNDARY_TOLERANCE = 0
+
+vertices = [
+    [SIZE, SIZE * 2, -SIZE],
+    [SIZE, 0, -SIZE],
+    [-SIZE, 0, -SIZE],
+    [-SIZE, SIZE * 2, -SIZE],
+    [SIZE, SIZE * 2, SIZE],
+    [SIZE, 0, SIZE],
+    [-SIZE, 0, SIZE],
+    [-SIZE, SIZE * 2, SIZE]
+]
+
+edges = [
+    (0, 1),
+    (2, 3),
+    (3, 0),
+    (4, 5),
+    (6, 7),
+    (7, 4),
+    (0, 4),
+    (3, 7)
+]
+
+tri_vertices = [
+    (0.0, 0, -SIZE),    # front vertex
+    (-SIZE/2, 0, 0),  # Bottom left vertex
+    (SIZE/2, 0, 0),   # Bottom right vertex
+    (0.0, SIZE/2, 0)    # Bottom top vertex
+]
+
+tri_edges = [
+    (0, 1),  # Top vertex to bottom left vertex
+    (0, 2),  # Top vertex to bottom right vertex
+    (0, 3),  # Top vertex to bottom front vertex
+    (1, 2),  # Bottom left vertex to bottom right vertex
+    (2, 3),  # Bottom right vertex to bottom front vertex
+    (3, 1)   # Bottom front vertex to bottom left vertex
+]
+
+class Game:
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+        self.key_pressed = False
+        self.env = DroneEnv()
+        self.model = PPO("MlpPolicy", self.env, verbose=1)
+
+
+    def run(self):
+        pygame.init()
+        display = (WIDTH, HEIGHT)
+        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+        glEnable(GL_DEPTH_TEST)
+
+        while True:
+            # Your existing game loop code (event handling, drawing, etc.)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+            self.update()
+
+
+
+    def update(self):
+        print("updating...")
+        # 1. Get current observation from environment
+        obs = self.env.get_observation()
+
+        # 2. Get AI action from trained model
+        action, _states = self.model.predict(obs)
+
+        # 3. Apply action and get new state
+        obs, reward, done, truncated, info = self.env.step(action)
+
+        # 4. Update elements
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self.update_view()
+
+        self.env.draw_ground_plane()
+        self.env.draw_goal_box()
+        for obstacle in self.env.obstacles:
+            obstacle.draw(vertices, edges)
+
+        # Update the display
+        pygame.display.flip()
+
+        self.clock.tick(60)
+
+    def update_view(self):
+        rotation_matrix = np.array([[np.cos(np.radians(self.env.drone.heading)), -np.sin(np.radians(self.env.drone.heading))],
+                                    [np.sin(np.radians(self.env.drone.heading)), np.cos(np.radians(self.env.drone.heading))]])
+        # Rotate the drone's shape based on the heading around its relative origin
+        glPushMatrix()
+        glTranslatef(*self.env.drone.position)
+        glRotatef(-self.env.drone.heading, 0, 1, 0)  # Rotate around the y-axis based on heading
+        self.env.drone.draw(tri_vertices, tri_edges)  # Draw the rotated drone shape
+        glPopMatrix()
+
+        camera_matrix = [0, 3]
+
+        # Define the camera position relative to the drone
+        rotated_camera = np.matmul(rotation_matrix, camera_matrix)
+
+        camera_position = (rotated_camera[0] + self.env.drone.position[0],
+                           self.env.drone.position[1],
+                           rotated_camera[1] + self.env.drone.position[2])
+
+        # Rotate the camera position with the drone
+        glLoadIdentity()
+        gluPerspective(60, (WIDTH / HEIGHT), 0.1, 100)
+        gluLookAt(camera_position[0], camera_position[1] + .5, camera_position[2],  # Camera position
+                  self.env.drone.position[0], self.env.drone.position[1], self.env.drone.position[2],  # Look at the drone
+                  0, 1, 0)  # Up vector
+    """
+    def reset_visualization(self):
+    # Reset drone position and heading
+    self.env.drone.position = [0, 0.2, 0]
+    self.env.drone.heading = 0
+    """
+
+    """
+    def handle_input(self, keys):
+    if any(keys):
+        self.key_pressed = True
+    else:
+        self.key_pressed = False
+
+    if keys[pygame.K_LEFT]:
+        self.drone.heading -= ROTATION_SPEED
+    if keys[pygame.K_RIGHT]:
+        self.drone.heading += ROTATION_SPEED
+
+    rotation_matrix = np.array([[np.cos(np.radians(self.drone.heading)), -np.sin(np.radians(self.drone.heading))],
+                                [np.sin(np.radians(self.drone.heading)), np.cos(np.radians(self.drone.heading))]])
+
+    if keys[pygame.K_a]:
+        delta_a = np.matmul(rotation_matrix, [-ACCELERATION, 0])
+        delta_v = [self.drone.velocity[0] + delta_a[0], self.drone.velocity[1], self.drone.velocity[2] + delta_a[1]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+    if keys[pygame.K_d]:
+        delta_a = np.matmul(rotation_matrix, [ACCELERATION, 0])
+        delta_v = [self.drone.velocity[0] + delta_a[0], self.drone.velocity[1], self.drone.velocity[2] + delta_a[1]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+    if keys[pygame.K_UP]:
+        delta_a = np.matmul(rotation_matrix, [0, -ACCELERATION]).tolist()
+        delta_v = [self.drone.velocity[0] + delta_a[0], self.drone.velocity[1], self.drone.velocity[2] + delta_a[1]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+    if keys[pygame.K_DOWN]:
+        delta_a = np.matmul(rotation_matrix, [0, ACCELERATION])
+        delta_v = [self.drone.velocity[0] + delta_a[0], self.drone.velocity[1], self.drone.velocity[2] + delta_a[1]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+    if keys[pygame.K_w]:
+        delta_v = [self.drone.velocity[0], self.drone.velocity[1] + ACCELERATION, self.drone.velocity[2]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+    if keys[pygame.K_s]:
+        delta_v = [self.drone.velocity[0], self.drone.velocity[1] - ACCELERATION, self.drone.velocity[2]]
+        self.drone.update_velocity(delta_v, MAX_VELOCITY)
+    """
+
+    def get_state(self):
+        # Example state representation
+        return np.array(self.drone.position + self.drone.velocity)
+
